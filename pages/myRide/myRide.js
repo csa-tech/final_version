af8d2c4900939764c86296b7aae6bccf8af1e851
Qinghang Hong <qhong@umail.ucsb.edu>
2019-04-29 00:55:19 -0700

    delete destination and use ui in departure

--- pages/myRide/myRide.js
+++ pages/myRide/myRide.js
@@ -18,17 +18,10 @@ Page({
     that.setData({
       user_ID: app.globalData.user_ID
     })
-    /*var response = [
-      { "rideid": "3","driverID":1, "departure": "Santa Barbara", "destination": "Los Angeles", "date": "01/08", "time": "7:00 pm", "status": "responded", "people": "4", "approved_people": "2" ,"driverName":"Hello","carType":"Nissan","price":"30"}, 
-      { "rideid": "3", "driverID": 2, "departure": "San Francisco", "destination": "Santa Barbara", "date": "01/07", "time": "8:00 am", "status": "pending", "people": "4", "approved_people": "2", "driverName": "Hello", "carType": "Toyota", "price": "40" },
-      { "rideid": "3", "driverID": 3, "departure": "Santa Barbara", "destination": "San Diego", "date": "01/10", "time": "6:00 pm", "people": "4", "approved_people": "2", "driverName": "Hello", "carType": "Benz", "price": "25" }
-    ]
-    that.setData({
-      response:response
-    })
-    */
+   
     wx.request({
-      url: "http://localhost:3000/listing?wechat_id=" + app.globalData.user_ID ,
+      //url: "http://13.56.241.40:3077/ride_list? +user_ID" + app.globalData.user_ID ,
+      url: "http://13.56.241.40:3077/ride_list?user_ID=1",
       data: {},
       header: {
         'content-type': 'application/json' // 默认值
@@ -42,48 +35,12 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onChange(event){
+    wx.navigateTo({
+      url: '../notification/notification'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
+ 
   /**
    * 用户点击右上角分享
    */
